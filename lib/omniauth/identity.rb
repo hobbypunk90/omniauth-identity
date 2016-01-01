c403b7d19c0c5fb65b5e2f5f591cff7c7d886190
Christoph Lupprich <christoph@luppri.ch>
2016-01-01 14:32:13 +0100

    Remove all adapters except ActiveRecord

--- lib/omniauth/identity.rb
+++ lib/omniauth/identity.rb
@@ -6,14 +6,10 @@ module OmniAuth
   end
 
   module Identity
-    autoload :Model,               'omniauth/identity/model'
-    autoload :SecurePassword,      'omniauth/identity/secure_password'
+    autoload :Model,          'omniauth/identity/model'
+    autoload :SecurePassword, 'omniauth/identity/secure_password'
     module Models
-      autoload :ActiveRecord,      'omniauth/identity/models/active_record'
-      autoload :MongoMapper,       'omniauth/identity/models/mongo_mapper'
-      autoload :Mongoid,           'omniauth/identity/models/mongoid'
-      autoload :DataMapper,        'omniauth/identity/models/data_mapper'
-      autoload :CouchPotatoModule, 'omniauth/identity/models/couch_potato'
+      autoload :ActiveRecord, 'omniauth/identity/models/active_record'
     end
   end
 end
